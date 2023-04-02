65abe366997d1db46050b45953724b9c8a0f9bb7
Sadboi <alexandermike1999@gmail.com>
2023-04-02 16:32:03 +0630

    Final commit

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,7 +1,7 @@
 const Heading = () => {
   return (
-    <div className="px-8 md:px-4 md:mt-28 md:text-center font-bold tracking-wider">
-      <h1 className="xl:text-4xl md:text-xl text-2xl lg:pb-3.5 p-3">
+    <div className="px-8 md:px-4 md:mt-28 lg:text-center font-bold tracking-wider">
+      <h1 className="xl:text-4xl md:text-xl text-2xl lg:pb-3.5 py-3">
         Your Choice Plans
       </h1>
       <p className="md:w-1/2 mx-auto xl:text-lg text-sm text-subHeading xl:mt-3.5 mt-2">
